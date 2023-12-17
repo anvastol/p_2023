e39491031562639ff69621c3dbb8952dd07fcc3e
anvastol@yandex.ru <anvastol@yandex.ru>
2023-12-17 12:10:57 +0300

    Added sentiment.py

---
+++ sentiment.py
@@ -0,0 +1,7 @@
+!pip install transformers sentencepiece
+
+from transformers import pipeline
+
+translator = pipeline("translation_ru_to_fr", "Helsinki-NLP/opus-mt-ru-fr")
+
+translator("Меня зовут Анастасия, я живу в Москве")
